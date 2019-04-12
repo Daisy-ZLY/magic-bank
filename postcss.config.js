3ee5d7b97b9a96c15c3d82372ce370b05c0bd2df
zhangliyuan <zhangliyuan0727@163.com>
2019-04-12 21:31:37 +0800

    commit config

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,10 @@
 module.exports = {
   plugins: {
-    autoprefixer: {}
+    autoprefixer: {},
+    "postcss-pxtorem": {
+      rootValue: 32, //The root element font size,2倍屏为32，1倍屏为16
+      propList: ["*"],
+      minPixelValue: 12
+    }
   }
 };
